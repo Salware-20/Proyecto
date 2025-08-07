322ddc98c873f5da5f8c8c2e50b4f0ecf64070d0
Andrés <maringusman11@gmail.com>
2025-08-06 21:11:36 -0600

    Agregar archivos iniciales: index.php, menu.php y style.css

---
+++ menu.php
@@ -0,0 +1,10 @@
+<nav class="menu">
+    <h1 class="titulo"><a href="#">Biblioteca Digital Universe</a></h1>
+    <ul class="links">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Catalogo</a></li>
+        <li><a href="#">Préstamos</a></li>
+        <li><a href="#">Panel Admin</a></li>
+        <li><a href="#">Registrarse</a></li>
+    </ul>
+</nav>
