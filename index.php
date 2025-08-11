8b83404a3e34b16b17bb6d009d0ae75407ae406b
Andrés <maringusman11@gmail.com>
2025-08-10 19:33:56 -0600

    Observaciones
    Esta viendo nada más

--- index.php
+++ index.php
@@ -4,9 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+    <title>Digital Universe</title>
+    <link rel="shortcut icon" href="Imagenes/Icono.png" type="image/jpge">
 </head>
-<body class="index">
-    <?php include 'menu.php';?>
+<body>
+<?php include 'menu.php'; ?>
+
+  <div class="Container">
+    <img src="Imagenes/fondoindex.png" alt="Fondo" class="imagen-fondo">
+
+    <div class="Texto">
+      <h1>Bienvenidos a la Biblioteca Digital Universe</h1>
+      <p>“Nuestra biblioteca digital permite a
+lectores y administradores gestionar libros, realizar préstamos y acceder a una amplia
+colección de títulos desde cualquier dispositivo.</p>
+
+    </div>
+  </div>
+
+
+<div class="gallery-container">
+  <div class="gallery">
+    <a target="_blank" href="login.php">
+      <img src="Imagenes/login.png" alt="Iniciar sesión">
+    </a>
+    <div class="desc">Iniciar sesión</div>
+  </div>
+
+  <div class="gallery">
+    <a target="_blank" href="registro.php">
+      <img src="Imagenes/registrarse.png" alt="Registrarse">
+    </a>
+    <div class="desc">Registrarse</div>
+  </div>
+
+  <div class="gallery">
+    <a target="_blank" href="catalogo.php">
+      <img src="Imagenes/catalogo.png" alt="Ver catálogos de libros">
+    </a>
+    <div class="desc">Ver catálogos de libros</div>
+  </div>
+</div>
+
+<?php 
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$basededatos = "biblioteca";
+
+$conexion = new mysqli($host, $usuario, $contrasena, $basededatos);
+ 
+?>
+
+
+
 </body>
 </html>
+
--- menu.php
+++ menu.php
@@ -2,8 +2,8 @@
     <h1 class="titulo"><a href="index.php">Biblioteca Digital Universe</a></h1>
     <ul class="links">
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="#">Catalogo</a></li>
-        <li><a href="#">Préstamos</a></li>
+        <li><a href="Catalogos/catalogo.php">Catalogo</a></li>
+        <li><a href="Prestamos/mis_prestamos.php">Préstamos</a></li>
         <li><a href="#">Panel Admin</a></li>
         <li><a href="registro.php">Registrarse</a></li>
     </ul>
